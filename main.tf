1b3d0a8a2869c3ccf0798975e0b73a7f05a1af3a
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-07-31 17:05:42 +0530

    added routes and related script

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "roboshop" {
-  source       = "../terraform-aws-vpc"
+  #source       = "../terraform-aws-vpc"
+  source       = "git::https://github.com/ravikumar-darsi/terraform-aws-vpc.git?ref=main"
   project_name = var.project_name
   environment  = var.environment
   common_tags  = var.common_tags
@@ -10,4 +11,10 @@ module "roboshop" {
 
   # private subnet
   private_subnets_cidr = var.private_subnets_cidr
+
+  # database subnet
+  database_subnets_cidr = var.database_subnets_cidr
+
+  #peering
+  is_peering_required = var.is_peering_required
 }  
