40df8629939a9b0908750ba097968c10dfe3346c
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-07-31 15:18:05 +0530

    vpc creation using terraform module

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.58.0" #AWS provider version, not terraform version
+    }
+  }
+
+  backend "s3" {
+    bucket         = "daws67s-remote-state" # you should creat this s3 bucket
+    key            = "vpc-test"
+    region         = "us-east-1"
+    dynamodb_table = "daws67s-locking" # you should create the table in dynamoDB
+  }
+}
+
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+} 
